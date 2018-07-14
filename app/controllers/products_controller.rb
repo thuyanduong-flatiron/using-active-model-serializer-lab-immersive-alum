5727c4f06ccb2c456da7144b9b89d9146f116f9a
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-14 11:21:22 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,8 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
+      format.json { render json: @product}
+      #.to_json(only: [:id, :name])
     end
   end
 
